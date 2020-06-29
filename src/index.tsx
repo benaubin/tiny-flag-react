162dfa87f87d36e4adba3033aac7a53cc1dbaa8b
Ben Aubin <ben@benaubin.com>
2020-06-29 17:44:52 -0500

    Rename countryName property to alt

--- src/index.tsx
+++ src/index.tsx
@@ -9,24 +9,26 @@ const supportsEmoji = ifEmoji("🇺🇸");
 export interface TinyFlagProps {
   /** ISO 3166-1 alpha-2 code */
   country: string;
-  /** Human-readable country name (used as image fallback) */
-  countryName: string;
+  /** Alt text to serve as the image fallback */
+  alt: string;
   /** Image to be used if flag emojis are not supported */
   fallbackImageURL: string;
 }
 
+/**
+ * An accessible emoji flag, with graceful fallbacks for platforms without support for flag emojis.
+ *
+ * Adjust sizing using font-size.
+ */
 export default function TinyFlag({
   country,
-  countryName,
+  alt,
   fallbackImageURL,
 }: TinyFlagProps) {
   if (supportsEmoji) {
     try {
       return (
-        <span
-          role="img"
-          aria-label={countryName}
-          style={{ position: "relative" }}>
+        <span role="img" aria-label={alt} style={{ position: "relative" }}>
           {tinyFlag(country)}
         </span>
       );
@@ -39,7 +41,7 @@ export default function TinyFlag({
       <img
         style={{ width: "1em", height: "1em" }}
         src={fallbackImageURL}
-        alt={countryName}></img>
+        alt={alt}></img>
     </>
   );
 }
